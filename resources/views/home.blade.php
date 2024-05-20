cbe35705c2b04ec0b8695453d478a0b5ffe41ab6
daniele-rosati <drosati151@gmail.com>
2024-05-20 22:02:58 +0200

    add menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>ciao soono il contenuto</h1>
+<div class="container dc-mainContainer">
+
+    <main class="row justify-content-center">
+
+        {{-- CARDS CONTAINER --}}
+        <div class="col-8 py-5">
+
+            <div class="row row-cols-6 g-3">
+
+                {{-- CARDS --}}
+                @foreach ($comics as $comic)
+    
+                    <div class="col-2">
+
+                        <div class="card bg-transparent">
+
+                            <div class="p-1 dc-coverContainer" style="background-image: url( {{ $comic ['thumb'] }} )">
+
+                            </div>
+        
+                            <div class="card-body p-0 pt-3">
+        
+                                <p class="card-title text-uppercase">{{ $comic ['series'] }}</p>
+                                <p class="card-text text-capitalize">{{ $comic ['type'] }}, price: {{ $comic ['price'] }}</p>
+        
+                            </div>
+        
+                        </div>
+                    
+                    </div>
+
+                @endforeach
+                {{-- END CARDS --}}
+
+                <div class="col-12 d-flex justify-content-center align-items-center">
+                <a href="#" class="btn text-uppercase px-5 dc-more">load more</a>
+                </div>
+
+            </div>
+
+        </div>
+        {{-- END CARDS CONTAINER --}}
+
+    </main>
+
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel Comics{{ Route::currentRouteName() == 'welcome' ? ' - Welcome' : ''}}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -17,11 +17,83 @@
 
 <body>
 
-   header
+    {{-- <header> --}}
 
-    <main>
+        <div class="container-fluid">
+
+            <div class="row justify-content-center">
+    
+                <div class="col-8 p-0">
+    
+                    <nav class="navbar navbar-expand-lg py-0">
+    
+                        <a class="navbar-brand flex-grow-1" href="#"><img src="images\jumbotron.jpg"></a>
+    
+                        <div class="navbar-nav d-flex justify-content-between text-uppercase">
+    
+                            <a class="nav-link px-4 py-5">
+                                link.text</a>
+    
+                        </div>
+    
+                    </nav>
+    
+                </div>
+    
+            </div>
+
+            <div class="row">
+    
+                <div class="col-12 d-flex justify-content-center p-0 dc-jumbotron">
+    
+                    <div class="col-8 position position-relative">
+    
+                        <div class=" position-absolute top-100 translate-middle-y py-2 px-4 dc-currentSeries">
+                            <h5 class="text-uppercase m-0">Current Series</h5>
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+    
+        </div>
+
+    {{-- </header> --}}
+
+
+    <main class="">
+
         @yield('content')
+
     </main>
+
+        {{-- MENU --}}
+
+        <div class="container-fluid dc-menuCont">
+
+            <div class="row justify-content-center">
+    
+                <div class="col-8 p-0">
+    
+                    <div class="d-flex align-items-center justify-content-evenly flex-wrap p-4 dcMenuEl">
+    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias porro tenetur quis deleniti voluptas, eius sit nemo odio magnam, totam at ea assumenda quasi tempora eos, nihil iure itaque voluptatibus.</p>
+    
+                        {{-- <a :href="link.path" class="text-uppercase m-3" v-for="link in servicesLinks"><img
+                                :src="" alt=""><span class="ms-4">{{ link.text }}</span></a> --}}
+    
+                    </div>
+    
+                </div>
+    
+            </div>
+    
+        </div>
+
+    {{-- FOOTER --}}
+
+   
 
 </body>
 
